4b1de4b9d03ce2eff1e5ebe5b7172fb1656f1de9
Michal Witko <m.witko2000@gmail.com>
2025-12-04 21:12:03 +0100

    fix

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,8 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import LazyVim extras (vue)
+    { import = "lazyvim.plugins.extras.lang.vue" },
     -- import/override with your plugins
     { import = "plugins" },
   },
